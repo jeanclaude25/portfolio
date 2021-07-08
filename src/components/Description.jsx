0e59d7d60073975414f0d809808b724d599a9fca
Hasham Vakani <vakanihasham64@gmail.com>
2021-07-08 17:10:10 +0500

    fix: fix description onclick props render error

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,86 +1,84 @@
-import React from 'react';
-import Video from './Video';
+import React from 'react'
+import Video from './Video'
 
 class Description extends React.Component {
+  title (a) { console.log(a); let title; try { title = a.name } catch (e) { title = '' };return title }
+  info (a) { let infos; try { infos = a.description.infos } catch (e) { infos = '' };return infos }
+  images (a) {
+    let images; try { images = a.description.images } catch (e) { images = '' };
+    const liste = []
+    try {
+      for (const i in images) {
+        liste.push(images[i])
+      };
+    } catch (e) { liste.push('') }
 
+    return liste
+  }
 
-        constructor (props){
-                            super(props)
-                            this.activeElement = props
-                            }
+  imagesfree (a) {
+    let images; try { images = a.description.imfreeext } catch (e) { images = '' };
+    const liste = []
+    try {
+      for (const i in images) {
+        liste.push(images[i])
+      };
+    } catch (e) { liste.push('') }
 
+    return liste
+  }
 
-    title(a){ console.log(a);let title;try{title = a.name}catch(e){title=""};return title}
-    info(a){ let infos;try{infos = a.description.infos;}catch(e){infos=""};return infos}
-    images(a){ let images;try{images = a.description.images;}catch(e){images = ""};
-        let liste = [];
-        try{
-        for(var i in images){
-            liste.push(images[i]);
-            };
-            }catch(e){liste.push("");}
+  render () {
+    const activeElement = this.props.props
+    console.log(activeElement.description.videos)
+    return (
+      <div className='description' id='desc_panel'>
 
-        return liste
-    }
-    imagesfree(a){ let images;try{images = a.description.imfreeext;}catch(e){images = ""};
-        let liste = [];
-        try{
-        for(var i in images){
-            liste.push(images[i]);
-            };}catch(e){liste.push("");}
+        <div className='title'><h1>{this.title(activeElement)}</h1></div>
+        <div className='explication'><p>{this.info(activeElement)}</p></div>
+        <hr />
+        <div className='images'>
+          {this.images(activeElement).map((im) => {
+            return (
 
-        return liste
-    }
+              <img
+                alt={activeElement.name}
+                src={activeElement.path +
+                            activeElement.description.path +
+                            im +
+                            activeElement.description.extension} key={im}
+              />
+            )
+          })}
+        </div>
+        <div className='imagesFreeExt'>
+          {this.imagesfree(activeElement).map((imfree) => {
+            return (
 
+              <img
+                alt={activeElement.name} src={activeElement.path +
+                            activeElement.description.path +
+                            imfree} key={imfree}
+              />
+            )
+          })}
+        </div>
 
-    render() {
-        const activeElement = this.activeElement.props;
-        console.log(activeElement.description.videos);
-        return <div className="description" id="desc_panel">
+        {activeElement.description.videos && <Video props={activeElement} key={activeElement.videos} />}
+        {/* {activeElement.description.gifs && } */}
+        <p>.</p><p />
 
-                <div className="title"><h1>{this.title(activeElement)}</h1></div>
-                <div className="explication"><p>{this.info(activeElement)}</p></div>
-                <hr/>
-                <div className="images">
-                    {this.images(activeElement).map((im)=>{
-                    return(
-                        
-                         <img alt={activeElement.name} 
-                         src={activeElement.path+
-                            activeElement.description.path+
-                            im+
-                            activeElement.description.extension} key={im}/>
-                    )
-                    })}
-                </div>
-                <div className="imagesFreeExt">
-                    {this.imagesfree(activeElement).map((imfree)=>{
-                    return(
-                        
-                         <img alt={activeElement.name} src={activeElement.path+
-                            activeElement.description.path+
-                            imfree} key={imfree}/>
-                    )
-                    })}
-                </div>
-                
-                 {activeElement.description.videos && <Video props={activeElement} key={activeElement.videos}/>}
-                 {/* {activeElement.description.gifs && } */}
-                    <p>.</p><p></p>
+        {activeElement.link !== undefined && <a href={activeElement.path + activeElement.link} target='viewer'><img alt={activeElement.name} src={activeElement.path + activeElement.imgApp} key={activeElement.imgApp} /></a>}
+        {/* {activeElement.link==undefined && <img src= {activeElement.img} />} */}
 
-                {activeElement.link!==undefined && <a href = {activeElement.path+activeElement.link} target ="viewer"><img alt={activeElement.name} src={activeElement.path+activeElement.imgApp} key={activeElement.imgApp}/></a> }
-                {/* {activeElement.link==undefined && <img src= {activeElement.img} />} */}
-                
+        {/* <iframe src={activeElement.link} sandbox=''/> */}
 
-                {/* <iframe src={activeElement.link} sandbox=''/> */}
-                
-                
-                
-                {/* <div className="button" id="button_L" onClick={()=> press_on_left()}></div>
+        {/* <div className="button" id="button_L" onClick={()=> press_on_left()}></div>
                 <div className="button" id="button_R" onClick={()=> press_on_right()}></div> */}
-                                                            
-            </div>
-    };
+
+      </div>
+    )
+  };
 };
 
-export default Description;
+export default Description
