5adc2cf919bc9bd8eea9967b44bcf1c63da2b958
Hasham Vakani <vakanihasham64@gmail.com>
2021-07-16 20:40:03 +0500

    feat: add dark mode functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import "./styles/index.scss";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './styles/index.scss'
+import 'react-toggle/style.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
